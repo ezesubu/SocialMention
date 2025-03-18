025fe501a67a75bbcd867f5993691d6606aec965
EazyGypsy <ezesubu@gmail.com>
2025-03-18 13:04:44 -0500

    Initial files from Test

---
+++ src/main/java/com/example/dtos/SocialMention.java
@@ -0,0 +1,13 @@
+package com.example.dtos;
+
+
+import java.util.List;
+
+public class SocialMention {
+    private String message;
+    private String facebookAccount;
+    private String tweeterAccount;
+    private String creationDate;
+    private String tweeterUrl;
+    private List<String> facebookComments;
+}
